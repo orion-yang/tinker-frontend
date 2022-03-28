59aba6ae0b722aeb5a913f0652fe6b87cd8537cf
orion-yang <osy29405@uga.edu>
2022-03-27 22:04:52 -0400

    Login Works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent } from './app.component';
 import { HomePageComponent } from './component/home-page/home-page.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginPageComponent } from './component/login-page/login-page.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { LoginPageComponent } from './component/login-page/login-page.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
