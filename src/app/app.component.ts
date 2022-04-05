a91f9e824f97a573a8d2031fb4619000eb426428
jdhubb99 <jacobdhubbard@gmail.com>
2022-04-05 03:03:37 -0400

    added temporary app component test to display users from the database

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { User } from './models/user/user';
+import { UserService } from './models/user/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'tinker-frontend';
+export class AppComponent implements OnInit {
+  title = 'app';
+  public users!: User[];
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  constructor(private userService: UserService) {}
+
+  // gets all users from the database
+  public getUsers(): void {
+    this.userService.getUsers().subscribe(
+      (response: User[]) => {
+        this.users = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
 }
--- src/app/models/user/user.service.ts
+++ src/app/models/user/user.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from './user';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class UserService {
-  private userServiceUrl = 'http://localhost:8080';
+  private userServiceUrl = environment.userServiceUrl;
 
   constructor(private http: HttpClient) { }
 
