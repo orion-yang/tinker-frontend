5b36368c997e96c893443ddf13dd50037640407e
orion-yang <osy29405@uga.edu>
2022-04-26 18:19:41 -0400

    ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,12 @@ import { HomePageComponent } from './component/home-page/home-page.component';
 import { FormsModule } from '@angular/forms';
 import { LoginPageComponent } from './component/login-page/login-page.component';
 import { ForgotPasswordComponent } from './component/forgot-password/forgot-password.component';
-import { ProjectsPageComponent } from './component/projects-page/projects-page.component';
 import { ProjectsComponent } from './component/projects/projects.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent},
   { path: 'login', component: LoginPageComponent},
   { path: 'forgot-password', component: ForgotPasswordComponent},
-  { path: 'projects-page', component: ProjectsPageComponent},
   { path: 'projects', component: ProjectsComponent}
 
 ];
